96c0c19dda4a75e7bb6437e87c25e1830034f22c
DMQ <duminqing@outlook.com>
2025-12-30 13:52:42 +0800

    1

--- rpa_tiktok/rpa_tiktok/tiktok_scrolling.py
+++ rpa_tiktok/rpa_tiktok/tiktok_scrolling.py
@@ -22,6 +22,7 @@ def perform_tiktok_scrolling(**kwargs):
     try:
         device = connect_device(device_id, pad_code, local_ip, local_port)
         open_tiktok(device)
+        search(device)
     except Exception as e:
         logger.error(f"{device_id}连接设备失败: {str(e)}")
         return {"status": "error", "message": f"连接设备失败: {str(e)}"}
@@ -58,6 +59,23 @@ def perform_tiktok_scrolling(**kwargs):
             continue
     press_home(device)
     return {"status": "success", "message": "Scrolling completed"}
+
+def search(device,**kwargs):
+    try:
+        click_bound(device, (912,75,1080,243))#[912,75][1080,243]
+        #//android.widget.EditText
+        # 方式2：简化写法（直接链式调用，若控件不存在会抛出异常，可按需使用）
+        device.xpath('//android.widget.EditText').set_text("pads")
+        random_sleep()
+        # search按钮[836,84][1080,216]  //*[@text="Search"]
+        click_bound(device, (836,84,1080,216))
+        # Videos tab[466,228][615,348]  //*[@text="Videos"]
+        device(text="Videos").click()
+        # 第一个视频 [24,372][528,1178]
+        logger.info(f"点击第一个视频")
+        click_bound(device, (24,372,528,1178))
+    except:
+        logger.error(f"点击搜索失败,{screenshot(device, "SEARCH_ERROR", **kwargs)}")
 
 def click_like(device,**kwargs):
     try:
